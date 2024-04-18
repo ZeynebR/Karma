0789edb4b7d4f328d72c0a400ce8470cd5ab6b75
ZeynebR <zeynabnr@code.edu.az>
2024-04-18 11:30:46 +0200

    task added

---
+++ Karma/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karma.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Karma/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Karma.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
